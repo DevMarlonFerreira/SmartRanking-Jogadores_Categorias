c9ebd9522a7425e77049c94bbc8e04db3ea69350
Marlon <devmarlonferreira@gmail.com>
2024-01-25 19:32:00 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    MongooseModule.forRoot(`${process.env.MONGODB_URL}`),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
