a097d6b6ea7dbf42a3a52abd8f2ece3dd6abcc79
Marlon <devmarlonferreira@gmail.com>
2024-02-01 14:04:13 -0300

    Update Main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
-import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoriasModule } from './categorias/categorias.module';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
     MongooseModule.forRoot(`${process.env.MONGODB_URL}`),
-    MongooseModule.forFeature([
-      { name: 'Categoria', schema: CategoriaSchema },
-      { name: 'Jogador', schema: JogadorSchema },
-    ]),
+    CategoriasModule,
+    JogadoresModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices'
+// import { Logger } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config'
+import * as momentTimezone from 'moment-timezone'
+
+// const logger = new Logger('Main')
+// const configService = new ConfigService()
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: [
-        process.env.AMQPS
-      ],
+      urls: [process.env.AMQPS],    
       noAck: false,
-      queue: 'admin-backend',
+      queue: 'admin-backend'
     },
   });
+
+  Date.prototype.toJSON = function(): any {
+    return momentTimezone(this)
+      .tz("America/Sao_Paulo")
+      .format("YYYY-MM-DD HH:mm:ss.SSS")
+  }
 
   await app.listen();
 }
